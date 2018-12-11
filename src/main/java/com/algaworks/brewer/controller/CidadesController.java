8f4af9c9903ecfc6ee9655742acc393fe32c6947
Douglas Dias <douglasmarquesdias@hotmail.com>
2018-12-10 22:28:49 -0200

    configurando_layouts

---
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -0,0 +1,17 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.algaworks.brewer.model.Cerveja;
+
+@Controller
+@RequestMapping("/brewer")
+public class CidadesController {
+
+	@RequestMapping("/cidades/novo")
+	public String novo(Cerveja cerveja) {
+		return "cidade/CadastroCidade";
+	}
+
+}
---
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -0,0 +1,17 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.algaworks.brewer.model.Cerveja;
+
+@Controller
+@RequestMapping("/brewer")
+public class EstiloController {
+
+	@RequestMapping("/estilos/novo")
+	public String novo(Cerveja cerveja) {
+		return "estilo/CadastroEstilo";
+	}
+
+}
