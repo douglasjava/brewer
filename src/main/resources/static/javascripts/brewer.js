83af88e5a44917235ed32eef744addb1cfd09ff5
Douglas Dias <douglasmarquesdias@hotmail.com>
2019-03-22 22:10:07 -0300

    otimizando-javaScript

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,29 @@
-$(function(){
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({ thousands: '.', decimal: ',', });
-	
-	var plain= $('.js-plain');
-	plain.maskMoney({ precision: 0, thousands: '.', allowZero: true });
+var Brewer = Brewer || {};
+
+Brewer.MaskMoney = (function() {
+
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+
+	MaskMoney.prototype.enable = function() {
+		this.decimal.maskMoney({
+			decimal : ',',
+			thousands : '.',
+		});
+		this.plain.maskMoney({
+			precision : 0,
+			thousands : '.'
+		});
+	}
+
+	return MaskMoney;
+
+}());
+
+$(function() {
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
+
 })
