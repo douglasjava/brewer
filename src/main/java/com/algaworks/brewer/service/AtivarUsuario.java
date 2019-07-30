b488c37151fd29badccbcbc7fbc34da6acdc4a8e
Douglas <douglasmarquesdias@hotmail.com>
2019-07-30 08:08:29 -0300

    conflitos

---
+++ src/main/java/com/algaworks/brewer/service/AtivarUsuario.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AtivarUsuario {
+
+	private Long[] codigos;
+	private StatusUsuario status;
+
+}
